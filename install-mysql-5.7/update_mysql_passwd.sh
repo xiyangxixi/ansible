a0a9cdcec6ac3e98a58fb631c2d81adfee57fe3c
xiyangxixi <34201743+xiyangxixi@users.noreply.github.com>
2018-07-03 14:11:04 +0800

    修改mysql数据库root密码以及赋予远程连接权限

--- install-mysql-5.7/update_mysql_passwd.sh
+++ install-mysql-5.7/update_mysql_passwd.sh
@@ -1,3 +1,6 @@
 #! //bin/bash
 
 /usr/local/mysql/bin/mysql -uroot -p`cat /var/log/mysqld.log |grep 'root@localhost' |awk -F '[ ]' '{print $11}'` -e "use mysql;update user set authentication_string =password('123456'), host = '%' where user = 'root';flush privileges;"
+
+
+# 修改root密码以及赋予远程连接权限 update user set authentication_string =password('123456'), host = '%' where user = 'root';
